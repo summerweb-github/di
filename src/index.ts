a9b233d35715b9b29a845ffccbeaae653b89b5ac
Andrey Myagkov <elfenliedmail@yahoo.com>
2025-10-23 10:09:46 +0300

    fix: binding export

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,10 @@ export { container, DIContainer } from './di-container';
 export { Scope, classMetadataKey } from './const';
 export { Logger } from './logger';
 export type { ClassMetadata, EnumObject } from './types';
+export { BindingKey, Binding } from './binding';
+export type {
+  ScopeEnum,
+  BindingSource,
+  BindingTypeEnum,
+  Newable,
+} from './binding';
